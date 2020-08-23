49ae430979c9005cf1bc45fd9907233435607be2
mattistern <mattistern1@gmail.com>
2020-08-23 20:24:14 +0300

    try to fix screen size

--- workey/lib/company_account/screens/profile_screen.dart
+++ workey/lib/company_account/screens/profile_screen.dart
@@ -3,25 +3,11 @@ import 'package:flutter/material.dart';
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints:
-          BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            color: Colors.black,
-            width: double.infinity,
-            height: 10,
-          ),
-          Container(
-            color: Colors.blue,
-            width: double.infinity,
-            height: 10,
-            //constraints: BoxConstraints(maxHeight: context.size.height * 0.5),
-          ),
-        ],
-      ),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      var a = MediaQuery.of(context).padding.top;
+      return Container(
+        child: Text(a.toString()),
+      );
+    });
   }
 }
