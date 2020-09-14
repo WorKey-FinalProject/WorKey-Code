891a25a8e2947dfb892b6272dd36f00e4ec497ba
Tomer <tomerhazan22@gmail.com>
2020-09-14 17:41:59 +0300

    update getCurrUserData

--- workey/lib/general/models/group_employee_model.dart
+++ workey/lib/general/models/group_employee_model.dart
@@ -3,20 +3,12 @@ import 'package:flutter/foundation.dart';
 class GroupEmployeeModel {
   String id;
   String workGroupId;
-  String email;
-  String firstName;
-  String lastName;
-  String profilePicture;
   String salary;
   String role;
 
   GroupEmployeeModel({
     @required this.id,
     @required this.workGroupId,
-    @required this.email,
-    @required this.firstName,
-    @required this.lastName,
-    this.profilePicture,
     this.salary,
     this.role,
   });
@@ -24,10 +16,6 @@ class GroupEmployeeModel {
   Map<String, Object> toJson() {
     return {
       'workGroupId': this.workGroupId,
-      'email': this.email,
-      'firstName': this.firstName,
-      'lastName': this.lastName,
-      'profilePicture': this.profilePicture,
       'salary': this.salary,
       'role': this.role,
     };
@@ -36,20 +24,12 @@ class GroupEmployeeModel {
   void fromJsonToObject(Map snapshot, String uid) {
     id = uid;
     workGroupId = snapshot['workGroupId'];
-    email = snapshot['email'];
-    firstName = snapshot['firstName'];
-    lastName = snapshot['lastName'];
-    profilePicture = snapshot['profilePicture'] ?? '';
     salary = snapshot['salary'] ?? '';
     role = snapshot['role'] ?? '';
   }
 
   void updateGroupEmployee(GroupEmployeeModel groupEmployeeModel) {
     this.workGroupId = groupEmployeeModel.workGroupId;
-    this.email = groupEmployeeModel.email;
-    this.firstName = groupEmployeeModel.firstName;
-    this.lastName = groupEmployeeModel.lastName;
-    this.profilePicture = groupEmployeeModel.profilePicture;
     this.salary = groupEmployeeModel.salary;
     this.role = groupEmployeeModel.role;
   }
