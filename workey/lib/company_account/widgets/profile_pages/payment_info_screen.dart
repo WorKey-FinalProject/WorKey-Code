0b1e27efe39f7d7b87a174a0c7d46b047744a78c
mattistern <mattistern1@gmail.com>
2020-09-22 19:21:18 +0300

    remove

--- workey/lib/company_account/widgets/profile_pages/payment_info_screen.dart
+++ workey/lib/company_account/widgets/profile_pages/payment_info_screen.dart
@@ -10,26 +10,118 @@ class PaymentInfoScreen extends StatefulWidget {
 }
 
 class _PaymentInfoScreenState extends State<PaymentInfoScreen> {
+  String cardNumber = "";
+  String cardHolderName = "";
+  String expiryDate = "";
+  String cvv = "";
+  bool showBack = false;
+
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        _focusNode.hasFocus ? showBack = true : showBack = false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: CreditCard(
-            cardNumber: "5450 7879 4864 7854",
-            cardExpiry: "10/25",
-            cardHolderName: "Card Holder",
-            cvv: "456",
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            height: 40,
+          ),
+          CreditCard(
+            cardNumber: cardNumber,
+            cardExpiry: expiryDate,
+            cardHolderName: cardHolderName,
+            cardType: CardType.visa,
+            cvv: cvv,
             bankName: "Axis Bank",
-            cardType: CardType
-                .masterCard, // Optional if you want to override Card Type
-            showBackSide: false,
+            showBackSide: showBack,
             frontBackground: CreditCardBackground.black,
             backBackground: CreditCardBackground.white,
             showShadow: true,
           ),
-        ),
-      ],
+          SizedBox(
+            height: 40,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(hintText: "Card Number"),
+                  maxLength: 19,
+                  onChanged: (value) {
+                    setState(() {
+                      cardNumber = value;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(hintText: "Card Expiry"),
+                  maxLength: 5,
+                  onChanged: (value) {
+                    setState(() {
+                      expiryDate = value;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(hintText: "Card Holder Name"),
+                  onChanged: (value) {
+                    setState(() {
+                      cardHolderName = value;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                child: TextFormField(
+                  decoration: InputDecoration(hintText: "CVV"),
+                  maxLength: 3,
+                  onChanged: (value) {
+                    setState(() {
+                      cvv = value;
+                    });
+                  },
+                  focusNode: _focusNode,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
