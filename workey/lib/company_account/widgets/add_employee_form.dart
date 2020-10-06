bb8639ff772bb7d70aeb9f6867ad2c938f29012a
mattistern <mattistern1@gmail.com>
2020-10-06 20:25:22 +0300

    Update add_employee_form.dart
    search

--- workey/lib/company_account/widgets/add_employee_form.dart
+++ workey/lib/company_account/widgets/add_employee_form.dart
@@ -22,7 +22,7 @@ class AddEmployeeForm extends StatefulWidget {
 class _AddEmployeeForm extends State<AddEmployeeForm> {
   // final workGroupLocationController = GoogleMapController;
   List<PersonalAccountModel> personalAccountList = [];
-  List<String> tempSearchList = [];
+  List<PersonalAccountModel> tempSearchList = [];
   final _employeeEmailController = TextEditingController();
   final _employeeSalaryController = TextEditingController();
   final _employeePositionController = TextEditingController();
@@ -39,7 +39,7 @@ class _AddEmployeeForm extends State<AddEmployeeForm> {
       personalAccountList.forEach((element) {
         if (element.email.startsWith(searchValue)) {
           setState(() {
-            tempSearchList.add(element.email);
+            tempSearchList.add(element);
           });
         }
       });
@@ -134,65 +134,132 @@ class _AddEmployeeForm extends State<AddEmployeeForm> {
                 ),
               ),
 
-              _textFieldForm(_employeeEmailController, 'Employee\'s Email'),
-              _textFieldForm(_employeeSalaryController, 'Employee\'s Salary'),
-              _textFieldForm(
-                  _employeePositionController, 'Employee\'s Position'),
-              // Container(
-              //   margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 14.0),
-              //   child: Row(
-              //     children: [
-              //       Container(
-              //         width: 100,
-              //         height: 100,
-              //         margin: EdgeInsets.only(top: 8.0, right: 10.0),
-              //         decoration: BoxDecoration(
-              //           border: Border.all(
-              //               width: 1, color: Theme.of(context).primaryColor),
-              //           borderRadius: BorderRadius.all(
-              //             Radius.circular(30),
-              //           ),
-              //         ),
-              //         child: _workGroupLogoController.text.isEmpty
-              //             ? Align(
-              //                 alignment: Alignment.center,
-              //                 child: Text('Enter a URL'))
-              //             : FittedBox(
-              //                 child:
-              //                     Image.network(_workGroupLogoController.text),
-              //                 fit: BoxFit.cover,
-              //               ),
-              //       ),
-              //       Expanded(
-              //         child: TextFormField(
-              //           controller: _workGroupLogoController,
-              //           keyboardType: TextInputType.url,
-              //           decoration: InputDecoration(labelText: 'Image URL'),
-              //           onSaved: (value) {
-              //             _workGroupLogoController.text = value;
-              //           },
-              //         ),
-              //       ),
-              //     ],
-              //   ),
-              // ),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    onPressed: () async {
-                      FocusScope.of(context).requestFocus(new FocusNode());
-                      await _addNewWorkGroup();
-                    },
-                    padding: EdgeInsets.symmetric(horizontal: 50),
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text("Done"),
-                  ),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                      key: Key('${tempSearchList[index]}'),
+                      direction: DismissDirection.endToStart,
+                      background: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(5.0),
+                            bottomRight: Radius.circular(5.0),
+                          ),
+                        ),
+                        margin: EdgeInsets.all(8.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(Icons.delete, color: Colors.white),
+                              Text('Remove',
+                                  style: TextStyle(color: Colors.white)),
+                            ],
+                          ),
+                        ),
+                      ),
+                      confirmDismiss: (DismissDirection direction) async {
+                        return await showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text("Delete Confirmation"),
+                              content: const Text(
+                                  "Are you sure you want to remove this employee?"),
+                              actions: <Widget>[
+                                FlatButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: const Text("Delete")),
+                                FlatButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(false),
+                                  child: const Text("Cancel"),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: GestureDetector(
+                        // onTap: () => Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => EmployeeDetailScreen(),
+                        //   ),
+                        // ),
+                        child: Card(
+                          elevation: 5,
+                          margin:
+                              EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: NetworkImage(
+                                  tempSearchList[index].profilePicture),
+                            ),
+                            // ProfilePicture(
+                            //   isEditable: false,
+                            //   size: 30,
+                            //   imageUrl: '', //employeesList[index].picture,
+                            // ),
+
+                            title: Text(
+                              '${tempSearchList[index].firstName}',
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                            subtitle: Text(
+                              '${tempSearchList[index].lastName}',
+                            ),
+                            trailing: MediaQuery.of(context).size.width > 460
+                                ? FlatButton.icon(
+                                    icon: Icon(Icons.person_outline_rounded),
+                                    label: Text('Employee details'),
+                                    onPressed:
+                                        null, //() => deleteTx(emp[index].id),
+                                    textColor: Theme.of(context).accentColor,
+                                  )
+                                : IconButton(
+                                    icon: Icon(Icons.person_outline_rounded),
+                                    color: Theme.of(context).accentColor,
+                                    onPressed: () =>
+                                        null //deleteTx(transactions[index].id),
+                                    ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: tempSearchList.length,
                 ),
-              )
+              ),
+              //           _textFieldForm(_employeeEmailController, 'Employee\'s Email'),
+              //           _textFieldForm(_employeeSalaryController, 'Employee\'s Salary'),
+              //           _textFieldForm(
+              //               _employeePositionController, 'Employee\'s Position'),
+
+              //           Flexible(
+              //             child: Padding(
+              //               padding: const EdgeInsets.all(8.0),
+              //               child: RaisedButton(
+              //                 onPressed: () async {
+              //                   FocusScope.of(context).requestFocus(new FocusNode());
+              //                   await _addNewWorkGroup();
+              //                 },
+              //                 padding: EdgeInsets.symmetric(horizontal: 50),
+              //                 elevation: 2,
+              //                 shape: RoundedRectangleBorder(
+              //                   borderRadius: BorderRadius.circular(20),
+              //                 ),
+              //                 child: Text("Done"),
+              //               ),
+              //             ),
+              //           )
             ],
           ),
         ),
