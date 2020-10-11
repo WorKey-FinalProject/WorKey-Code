05ef4b4b69f15e99b40521dd41aeab4c824b1de0
Yotam_d <44746421+Yotam124@users.noreply.github.com>
2020-10-12 01:29:40 +0300

    fixes

--- workey/lib/personal_account/widgets/icons_grid_view.dart
+++ workey/lib/personal_account/widgets/icons_grid_view.dart
@@ -7,9 +7,7 @@ import 'package:workey/general/providers/auth.dart';
 import 'package:workey/general/providers/company_groups.dart';
 import 'package:workey/general/widgets/auth/signup_type.dart';
 
-
 import 'package:workey/general/widgets/location_input.dart';
-
 
 import 'package:workey/personal_account/screens/members_list_screen.dart';
 import 'package:workey/personal_account/screens/weekly_shifts_screen.dart';
@@ -78,6 +76,7 @@ class _IconsGridViewState extends State<IconsGridView> {
       case ButtonType.location:
         {
           showBottomSheet(
+            elevation: 5,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.vertical(top: Radius.circular(36.0)),
             ),
