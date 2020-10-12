3325061cb25a5aadb3285a686ce8eba9c5cc7552
mattistern <mattistern1@gmail.com>
2020-10-13 01:18:59 +0300

    Update index.js

--- workey/functions/index.js
+++ workey/functions/index.js
@@ -9,61 +9,95 @@ admin.initializeApp();
 
 var db = admin.database();
 
-exports.shitfNotifiction = functions.database.ref('/Company Groups/{companyId}/employeeList/{employeeId}/isWorking')
-.onUpdate(async (change, context) =>{
-    const companyId = context.params.companyId;
-    const employeeId = context.params.employeeId;
+exports.shitfNotification = functions.database.ref('/Company Groups/{companyId}/employeeList/{employeeId}/isWorking')
+    .onUpdate(async (change, context) => {
+        const companyId = context.params.companyId;
+        const employeeId = context.params.employeeId;
 
-    const getWorkGroupId = db
-        .ref(`/Company Groups/${companyId}/employeeList/${employeeId}/workGroupId`)
-            .once('value');
+        const getWorkGroupId = db
+            .ref(`/Company Groups/${companyId}/employeeList/${employeeId}/workGroupId`)
+                .once('value');
 
-    const getToken = db
-        .ref(`/Users/Company Accounts/${companyId}/token`)
-            .once('value');
+        const getToken = db
+            .ref(`/Users/Company Accounts/${companyId}/token`)
+                .once('value');
 
-    const getEmployeeFirstName = db
-        .ref(`/Users/Personal Accounts/${employeeId}/firstName`)
-            .once('value');
+        const getEmployeeFirstName = db
+            .ref(`/Users/Personal Accounts/${employeeId}/firstName`)
+                .once('value');
 
-    const getEmployeeLastName = db
-        .ref(`/Users/Personal Accounts/${employeeId}/lastName`)
-            .once('value');
+        const getEmployeeLastName = db
+            .ref(`/Users/Personal Accounts/${employeeId}/lastName`)
+                .once('value');
 
-    const promiseList = await Promise.all(
-        [getWorkGroupId,getToken,getEmployeeFirstName,getEmployeeLastName]
-    );
-    const workGroupId = promiseList[0].val();
-    let token = promiseList[1];
-    let employeeFirstName = promiseList[2].val();
-    let employeeLastName = promiseList[3].val();
+        const promiseList = await Promise.all(
+            [getWorkGroupId,getToken,getEmployeeFirstName,getEmployeeLastName]
+        );
+        const workGroupId = promiseList[0].val();
+        let token = promiseList[1];
+        let employeeFirstName = promiseList[2].val();
+        let employeeLastName = promiseList[3].val();
 
-    const getWorkGroupName = db
-        .ref(`/Company Groups/${companyId}/workGroupList/${workGroupId}/workGroupName`)
-            .once('value');
+        const getWorkGroupName = db
+            .ref(`/Company Groups/${companyId}/workGroupList/${workGroupId}/workGroupName`)
+                .once('value');
 
-    const workGroupNamePromise = await Promise.all([getWorkGroupName]);
-    let workGroupName = workGroupNamePromise[0].val();
+        const workGroupNamePromise = await Promise.all([getWorkGroupName]);
+        let workGroupName = workGroupNamePromise[0].val();
 
-    if(change.after.val()){
+        if(change.after.val()){
+            const payload = admin.messaging.MessagingPayload = {
+                notification: {
+                    title: `${workGroupName}`,
+                    body: `${employeeFirstName} ${employeeLastName} entered work`,
+                    click_action: 'FLUTTER_NOTIFICATION_CLICK',
+                }
+            };
+            return admin.messaging().sendToDevice(token.val(), payload);
+        }
         const payload = admin.messaging.MessagingPayload = {
             notification: {
                 title: `${workGroupName}`,
-                body: `${employeeFirstName} ${employeeLastName} entered work`,
+                body: `${employeeFirstName} ${employeeLastName} exited work`,
                 click_action: 'FLUTTER_NOTIFICATION_CLICK',
             }
         };
         return admin.messaging().sendToDevice(token.val(), payload);
-    }
-    const payload = admin.messaging.MessagingPayload = {
-        notification: {
-            title: `${workGroupName}`,
-            body: `${employeeFirstName} ${employeeLastName} exited work`,
-            click_action: 'FLUTTER_NOTIFICATION_CLICK',
-        }
-    };
-    return admin.messaging().sendToDevice(token.val(), payload);
-},);
+    },
+);
+
+exports.feedNotification = functions.database.ref('/Compamy Groups/{companyId}/feedList')
+    .onDelete(async (change, context) => {
+        const companyId = context.params.companyId;
+
+        const dd = db
+            .ref('/Users/Personal/Account').once('value');
+        
+        const p = await Promise.all([dd]);
+
+        let pp = p[0];
+
+        return console.log(pp);
+
+
+
+    //     var myEmployessList = db.ref('/Users/Personal Accounts/').orderByKey;
+    //     myEmployessList.on("value", function(snapshot) {
+    //       snapshot.forEach(function(childSnapshot) {
+    //         if(childData.val.equalTo(companyId)){
+
+    //         let key = childSnapshot.key;
+    //         var childData = childSnapshot.val();
+
+    //         console.log(key, childData);
+    //         }
+    //       });
+    //     }, function(error) {
+    //       console.error(error);
+    //     });
+    //    // return console.log(key, childData);
+    },
+);
 
 
 // // Create and Deploy Your First Cloud Functions
